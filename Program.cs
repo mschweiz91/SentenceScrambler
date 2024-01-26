2a786588c93615105e6f7e894c9800e76a804061
Mike Schweizer <maschw32836@gmail.com>
2024-01-26 15:14:09 -0500

    Initial console program commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a paragraph to rearrange:");
+        string paragraph = Console.ReadLine();
+
+        string shuffledParagraph = RandomlyRearrangeWords(paragraph);
+        Console.WriteLine("\nRearranged paragraph:");
+        Console.WriteLine(shuffledParagraph);
+        Console.ReadLine(); // Keep the console window open
+    }
+
+    static string RandomlyRearrangeWords(string paragraph)
+    {
+        // Split the paragraph into words
+        string[] words = paragraph.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Shuffle the words randomly
+        Random rng = new Random();
+        string[] shuffledWords = words.OrderBy(word => rng.Next()).ToArray();
+
+        // Join the shuffled words back into a new paragraph
+        string shuffledParagraph = string.Join(" ", shuffledWords);
+        return shuffledParagraph;
+    }
+}
